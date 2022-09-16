d37fd10fbbb733703c2acb0606bab99be77d86c8
Webservices <webservices@lorop.de>
2022-09-16 09:31:51 +0200

    websocket server und benötigte methoden erstellt

---
+++ src/message/message.gateway.ts
@@ -0,0 +1,88 @@
+import { WebSocketGateway, SubscribeMessage, MessageBody, ConnectedSocket, WebSocketServer } from '@nestjs/websockets';
+import {Server, Socket} from 'socket.io'
+import { MessageService } from './message.service';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { UpdateMessageDto } from './dto/update-message.dto';
+
+/**
+ * websocket server
+ * die einzelnen Metoden hören auf spezifiche Events
+ * die namen der Events, werden in den  @SubscribeMessage Annotationen gesetzt
+ */
+@WebSocketGateway()
+export class MessageGateway {
+  @WebSocketServer()
+  server: Server
+  
+  constructor(private readonly messageService: MessageService) {}
+
+  /**
+   * private 1:1 nachrichten werden verarbeitet und gespeichert
+   * @param createMessageDto 
+   * @param client 
+   * @returns 
+   */
+  @SubscribeMessage('createMessage')
+  async createMsg(@MessageBody() createMessageDto: CreateMessageDto, @ConnectedSocket() client: Socket) {
+    return this.messageService.create(createMessageDto);
+  }
+  
+  /**
+   * wenn ein client eine nachricht empfangen hat sendet er eine bestätigung,
+   * diese wird hier verarbeitet und der sender wird über den empfang der Nachricht informiert
+   * @param createMessageDto 
+   * @param client 
+   * @returns 
+   */
+  @SubscribeMessage('confirmMsg')
+  async confirmMsg(@MessageBody() createMessageDto: CreateMessageDto, @ConnectedSocket() client: Socket) {
+    return this.messageService.create(createMessageDto);
+  }
+
+  /**
+   * es werden alle nachrichten mit bestimmten sender und empfänger von der datenbank abgerufen
+   * hauptsächlich für chatverläufe
+   * @param createMessageDto 
+   * @param client 
+   * @returns 
+   */
+  @SubscribeMessage('findAllMessage')
+  async findAllMsgs(@MessageBody() createMessageDto: CreateMessageDto, @ConnectedSocket() client: Socket) {
+    return this.messageService.findAll();
+  }
+
+  /**
+   * diese methode gibt alle clients wieder die momentan mit dem server verbunden sind
+   * @param createMessageDto 
+   * @param client 
+   * @returns 
+   */
+  @SubscribeMessage('findOnlineUsers')
+  async findOnlineUsers(@MessageBody() createMessageDto: CreateMessageDto, @ConnectedSocket() client: Socket) {
+    return 3;
+  }
+
+  /**
+   * wenn sich ein client mit dem server verbindet,
+   * wird es allen anderen verbundenen clients gebroadcastet
+   * @param createMessageDto 
+   * @param client 
+   * @returns 
+   */
+  @SubscribeMessage('login')
+  async login(@MessageBody() createMessageDto: CreateMessageDto, @ConnectedSocket() client: Socket) {
+    return 3;
+  }
+
+  /**
+   * wenn sich ein client vom server trennt,
+   * wird es allen anderen verbundenen clients gebroadcastet
+   * @param createMessageDto 
+   * @param client 
+   * @returns 
+   */
+  @SubscribeMessage('logout')
+  async logout(@MessageBody() createMessageDto: CreateMessageDto, @ConnectedSocket() client: Socket) {
+    return 3;
+  }
+}
